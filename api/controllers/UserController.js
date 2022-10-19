0033ccac344f079676b8fe15dd8f42970a2f093c
César Guerra <warderer@gmail.com>
2022-10-19 16:52:57 -0500

    ✨ userController.signup with JOI

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,15 +5,34 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const joi = require('joi');
+
 module.exports = {
 
   /**
     * `UserController.signup()`
     */
   signup: async function (req, res) {
-    return res.json({
-      todo: 'signup() is not implemented yet!'
-    });
+    try {
+      const schema = joi.object({
+        email: joi.string().required().email(),
+        password: joi.string().required(),
+        firstName: joi.string().required(),
+        lastName: joi.string().required(),
+        gender: joi.string().valid('M','F','O'),
+        role: joi.string().valid('CUSTOMER','ADMIN'),
+        birthDate: joi.date(),
+        isActive: joi.boolean()
+      });
+      const { email, password, firstName, lastName, gender, role, birthDate } = await schema.validateAsync(req.allParams());
+      const user = await User.create({email, password, firstName, lastName, gender, role, birthDate}).fetch();
+      return res.ok(user);
+    } catch (err) {
+      if (err.name === 'ValidationError') {
+        return res.badRequest({err}).json();
+      }
+      return res.serverError(err).json();
+    }
   },
 
   /**
@@ -21,8 +40,9 @@ module.exports = {
     */
   login: async function (req, res) {
     return res.json({
-      todo: 'login() is not implemented yet!'
+      todo: 'signup() is not implemented yet!'
     });
+
   }
 };
 
